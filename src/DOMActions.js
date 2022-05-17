103f08c1c377f8a1ea3a61b9ae6f0a6d6b0ef392
sebastian.wan04 <sebastian.wan04@gmail.com>
2022-05-17 11:21:43 +0200

    Added missing semicolons and improved code cleanliness

--- src/DOMActions.js
+++ src/DOMActions.js
@@ -1,12 +1,12 @@
-const _getDOMElem = id => {
+const _getDOMElem = (id) => {
     return document.getElementById(id);
-}
+};
 
-export const mapListToDOMElements = listOfId => {
+export const mapListToDOMElements = (listOfId) => {
     const _viewElems = {};
 
     for (const id of listOfId) {
         _viewElems[id] = _getDOMElem(id);
     }
     return _viewElems;
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { getWeatherByCity } from './apiService.js'
-import { mapListToDOMElements } from './DOMActions.js';
-
+import { getWeatherByCity } from "./apiService.js";
+import { mapListToDOMElements } from "./DOMActions.js";
 
 class weatherApp {
     constructor() {
@@ -11,82 +10,86 @@ class weatherApp {
     initializeApp = () => {
         this.connectDOMElements();
         this.setupListeners();
-    }
+    };
 
     connectDOMElements = () => {
-        const listOfIds = Array.from(document.querySelectorAll('[id]')).map(elem => elem.id);
+        const listOfIds = Array.from(document.querySelectorAll("[id]")).map(
+            (elem) => elem.id
+        );
         this.viewElems = mapListToDOMElements(listOfIds);
-    }
+    };
 
     setupListeners = () => {
-        this.viewElems.searchInput.addEventListener('keydown', this.handleSubmit);
-        this.viewElems.searchButton.addEventListener('click', this.handleSubmit);
-        this.viewElems.returnToSearchBtn.addEventListener('click', this.returnToSearch);
-    }
+        this.viewElems.searchInput.addEventListener("keydown", this.handleSubmit);
+        this.viewElems.searchButton.addEventListener("click", this.handleSubmit);
+        this.viewElems.returnToSearchBtn.addEventListener("click", this.returnToSearch);
+    };
 
-    handleSubmit = event => {
-        if (event.type === 'click' || event.key === 'Enter') {
+    handleSubmit = (event) => {
+        if (event.type === "click" || event.key === "Enter") {
             this.fadeInOut();
             let query = this.viewElems.searchInput.value;
-            this.viewElems.errorInfo.style.display = "none"
+            this.viewElems.errorInfo.style.display = "none";
             getWeatherByCity(query)
-                .then(data => {
+                .then((data) => {
                     this.displayWeatherData(data);
-                    this.viewElems.searchInput.style.borderColor = 'black';
-
-                }).catch(() => {
+                    this.viewElems.searchInput.style.borderColor = "black";
+                })
+                .catch(() => {
                     this.fadeInOut();
-                    this.viewElems.errorInfo.style.display = 'block'
-                    this.viewElems.searchInput.style.borderColor = 'red';
+                    this.viewElems.errorInfo.style.display = "block";
+                    this.viewElems.searchInput.style.borderColor = "red";
                     this.viewElems.errorInfo.style.color = "red";
                     this.viewElems.errorInfo.innerText = "Incorrect name of the city";
-                })
+                });
         }
-    }
-
+    };
 
     fadeInOut = () => {
-        if (this.viewElems.mainContainer.style.opacity === '1' || this.viewElems.mainContainer.style.opacity === '') {
-            this.viewElems.mainContainer.style.opacity = '0';
+        if (
+            this.viewElems.mainContainer.style.opacity === "1" ||
+            this.viewElems.mainContainer.style.opacity === ""
+        ) {
+            this.viewElems.mainContainer.style.opacity = "0";
         } else {
-            this.viewElems.mainContainer.style.opacity = '1';
+            this.viewElems.mainContainer.style.opacity = "1";
         }
-    }
+    };
 
     switchView = () => {
-        if (this.viewElems.weatherSearchView.style.display !== 'none') {
-            this.viewElems.weatherSearchView.style.display = 'none';
-            this.viewElems.weatherForecastView.style.display = 'block';
+        if (this.viewElems.weatherSearchView.style.display !== "none") {
+            this.viewElems.weatherSearchView.style.display = "none";
+            this.viewElems.weatherForecastView.style.display = "block";
         } else {
-            this.viewElems.weatherSearchView.style.display = 'flex';
-            this.viewElems.weatherForecastView.style.display = 'none';
+            this.viewElems.weatherSearchView.style.display = "flex";
+            this.viewElems.weatherForecastView.style.display = "none";
         }
-    }
+    };
 
     returnToSearch = () => {
-        this.viewElems.searchInput.value = ''
+        this.viewElems.searchInput.value = "";
         this.fadeInOut();
         setTimeout(() => {
             this.switchView();
             this.fadeInOut();
         }, 500);
-    }
+    };
 
     displayWeatherData = (data) => {
         this.switchView();
         this.fadeInOut();
 
-        const weather = data.consolidated_weather[0]
+        const weather = data.consolidated_weather[0];
         this.viewElems.weatherCity.innerText = data.title;
         this.viewElems.weatherIcon.src = `https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`;
         this.viewElems.weatherIcon.alt = weather.weather_state_name;
-        this.viewElems.weatherCurrentTemp.innerText = 'Current temperature: ' + weather.the_temp.toFixed(2) + " °C";
-        this.viewElems.weatherMaxTemp.innerText = 'Max temperature: ' + weather.max_temp.toFixed(2) + " °C";
-        this.viewElems.weatherMinTemp.innerText = 'Min temperature: ' + weather.min_temp.toFixed(2) + " °C";
-
-    }
-
+        this.viewElems.weatherCurrentTemp.innerText =
+            "Current temperature: " + weather.the_temp.toFixed(2) + " °C";
+        this.viewElems.weatherMaxTemp.innerText =
+            "Max temperature: " + weather.max_temp.toFixed(2) + " °C";
+        this.viewElems.weatherMinTemp.innerText =
+            "Min temperature: " + weather.min_temp.toFixed(2) + " °C";
+    };
 }
 
-
-document.addEventListener('DOMContentLoaded', new weatherApp);
+document.addEventListener("DOMContentLoaded", new weatherApp());
